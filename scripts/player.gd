9c7effeca0b5a4ae6d01d7534d896a51a4b521ca
Helheimr1711 <david.ostry09@gmail.com>
2025-05-13 21:16:23 +0200

    Not finished character movement and camera
    Not finished

---
+++ scripts/player.gd
@@ -0,0 +1,45 @@
+extends CharacterBody3D
+
+@onready var camera_rig = $CameraRig
+
+## Basic variables for movement exported to easily change in  inspector
+# Speed variable in meters per second
+@export var speed = 14
+
+## Other variables
+# Target velocity
+var target_velocity = Vector3.ZERO
+
+
+## Movement
+# Movement direction based on inputs
+func _physics_process(delta):
+	# Variable for storing input direction
+	var direction = Vector3.ZERO
+	# Variable for rotation saving
+	var camera_rotation = camera_rig.rotation
+	
+	# Checking for inputs
+	if Input.is_action_just_pressed("move_right"):
+		camera_rig.rotate_y(deg_to_rad(-90))
+		print(rad_to_deg(camera_rotation.y))
+	elif Input.is_action_just_pressed("move_left"):
+		camera_rig.rotate_y(deg_to_rad(90))
+		print(rad_to_deg(camera_rotation.y))
+	elif Input.is_action_pressed("move_back"):
+		direction.z += 1
+	elif Input.is_action_pressed("move_forward"):
+		direction.z -= 1
+	
+	# Checking if the direction value has changed by inputs
+	if direction != Vector3.ZERO:
+		# Normalizing direction value
+		direction = direction.normalized()
+	
+	# Ground velocity
+	target_velocity.x = direction.x * speed
+	target_velocity.z = direction.z * speed
+	
+	# Finnaly character moving
+	velocity = target_velocity
+	move_and_slide()
