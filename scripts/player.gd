36f81e89e74c3d177ec2054940f03e09ee5a9e12
Helheimr1711 <david.ostry09@gmail.com>
2025-05-15 20:50:15 +0200

    Movement 1.1
    Better movement script, not using if statements for every direction

--- scripts/player.gd
+++ scripts/player.gd
@@ -16,6 +16,24 @@ var target_velocity = Vector3.ZERO
 func _physics_process(delta):
 	# Variable for storing input direction
 	var direction = Vector3.ZERO
+	# Variables for movement commands
+	var moving_commands_back = {
+		Vector3(0,   0,    0): {"Val": [{"Dir": "z"}, {"Num": 1}]},
+		Vector3(0,  90,    0): {"Val": [{"Dir": "x"}, {"Num": 1}]},
+		Vector3(0, 180,    0): {"Val": [{"Dir": "z"}, {"Num": -1}]},
+		Vector3(0, 270,    0): {"Val": [{"Dir": "x"}, {"Num": -1}]},
+		Vector3(0, -90,    0): {"Val": [{"Dir": "x"}, {"Num": -1}]},
+		Vector3(0, -180,   0): {"Val": [{"Dir": "z"}, {"Num": -1}]},
+		Vector3(0, -270,   0): {"Val": [{"Dir": "x"}, {"Num": 1}]}}
+	var moving_commands_forward = {
+		Vector3(0,    0,    0): {"Val": [{"Dir": "z"}, {"Num": -1}]},
+		Vector3(0,   90,    0): {"Val": [{"Dir": "x"}, {"Num": -1}]},
+		Vector3(0,  180,    0): {"Val": [{"Dir": "z"}, {"Num":  1}]},
+		Vector3(0,  270,    0): {"Val": [{"Dir": "x"}, {"Num":  1}]},
+		Vector3(0,  -90,    0): {"Val": [{"Dir": "x"}, {"Num":  1}]},
+		Vector3(0, -180,    0): {"Val": [{"Dir": "z"}, {"Num":  1}]},
+		Vector3(0, -270,    0): {"Val": [{"Dir": "x"}, {"Num": -1}]}}
+
 	# Variable for rotation saving
 	var camera_rotation = round(camera_rig.rotation_degrees)
 	
@@ -27,47 +45,21 @@ func _physics_process(delta):
 		camera_rig.rotate_y(deg_to_rad(90))
 		print(rad_to_deg(camera_rotation.y))
 	elif Input.is_action_just_pressed("move_back"):
-		if round(camera_rotation) == Vector3(0, 0, 0):
-			direction.z += 1
-		elif round(camera_rotation) == Vector3(0, 90, 0):
-			direction.x += 1
-			print(camera_rotation)
-		elif round(camera_rotation) == Vector3(0, 180, 0):
-			direction.z -= 1
-			print(camera_rotation)
-		elif round(camera_rotation) == Vector3(0, 270, 0):
-			direction.x -= 1
-			print(camera_rotation)
-		elif round(camera_rotation) == Vector3(0, -90, 0):
-			direction.x -= 1
-			print(camera_rotation)
-		elif round(camera_rotation) == Vector3(0, -180, 0):
-			direction.z -= 1
-			print(camera_rotation)
-		elif round(camera_rotation) == Vector3(0, -270, 0):
-			direction.x += 1
+		var direction_back = moving_commands_back[round(camera_rotation)]["Val"][0]["Dir"]
+		var num_back = moving_commands_back[round(camera_rotation)]["Val"][1]["Num"]
+		match direction_back:
+			"x":
+				direction.x += num_back
+			"z":
+				direction.z += num_back
 	elif Input.is_action_just_pressed("move_forward"):
-		if camera_rotation == Vector3(0, 0, 0):
-			direction.z -= 1
-			print(camera_rotation)
-		elif camera_rotation == Vector3(0, 90, 0):
-			direction.x -= 1
-			print(camera_rotation)
-		elif camera_rotation == Vector3(0, 180, 0):
-			direction.z += 1
-			print(camera_rotation)
-		elif camera_rotation == Vector3(0, 270, 0):
-			direction.x += 1
-			print(camera_rotation)
-		elif camera_rotation == Vector3(0, -90, 0):
-			direction.x +=1
-			print(camera_rotation)
-		elif camera_rotation == Vector3(0, -180, 0):
-			direction.z += 1
-			print(camera_rotation)
-		elif camera_rotation == Vector3(0, -270, 0):
-			direction.x -= 1
-			print(camera_rotation)
+		var direction_forward = moving_commands_forward[round(camera_rotation)]["Val"][0]["Dir"]
+		var num_forward = moving_commands_forward[round(camera_rotation)]["Val"][1]["Num"]
+		match direction_forward:
+			"x":
+				direction.x += num_forward
+			"z":
+				direction.z += num_forward
 	
 	# Checking if the direction value has changed by inputs
 	if direction != Vector3.ZERO:
