349c8016d469c546aa4dd59aa216ac58b5a3d872
Helheimr1711 <david.ostry09@gmail.com>
2025-05-15 15:08:03 +0200

    Movement is working
    Hell yeah

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,7 +4,7 @@ extends CharacterBody3D
 
 ## Basic variables for movement exported to easily change in  inspector
 # Speed variable in meters per second
-@export var speed = 14
+@export var speed = 200
 
 ## Other variables
 # Target velocity
@@ -17,7 +17,7 @@ func _physics_process(delta):
 	# Variable for storing input direction
 	var direction = Vector3.ZERO
 	# Variable for rotation saving
-	var camera_rotation = camera_rig.rotation
+	var camera_rotation = round(camera_rig.rotation_degrees)
 	
 	# Checking for inputs
 	if Input.is_action_just_pressed("move_right"):
@@ -26,10 +26,48 @@ func _physics_process(delta):
 	elif Input.is_action_just_pressed("move_left"):
 		camera_rig.rotate_y(deg_to_rad(90))
 		print(rad_to_deg(camera_rotation.y))
-	elif Input.is_action_pressed("move_back"):
-		direction.z += 1
-	elif Input.is_action_pressed("move_forward"):
-		direction.z -= 1
+	elif Input.is_action_just_pressed("move_back"):
+		if round(camera_rotation) == Vector3(0, 0, 0):
+			direction.z += 1
+		elif round(camera_rotation) == Vector3(0, 90, 0):
+			direction.x += 1
+			print(camera_rotation)
+		elif round(camera_rotation) == Vector3(0, 180, 0):
+			direction.z -= 1
+			print(camera_rotation)
+		elif round(camera_rotation) == Vector3(0, 270, 0):
+			direction.x -= 1
+			print(camera_rotation)
+		elif round(camera_rotation) == Vector3(0, -90, 0):
+			direction.x -= 1
+			print(camera_rotation)
+		elif round(camera_rotation) == Vector3(0, -180, 0):
+			direction.z -= 1
+			print(camera_rotation)
+		elif round(camera_rotation) == Vector3(0, -270, 0):
+			direction.x += 1
+	elif Input.is_action_just_pressed("move_forward"):
+		if camera_rotation == Vector3(0, 0, 0):
+			direction.z -= 1
+			print(camera_rotation)
+		elif camera_rotation == Vector3(0, 90, 0):
+			direction.x -= 1
+			print(camera_rotation)
+		elif camera_rotation == Vector3(0, 180, 0):
+			direction.z += 1
+			print(camera_rotation)
+		elif camera_rotation == Vector3(0, 270, 0):
+			direction.x += 1
+			print(camera_rotation)
+		elif camera_rotation == Vector3(0, -90, 0):
+			direction.x +=1
+			print(camera_rotation)
+		elif camera_rotation == Vector3(0, -180, 0):
+			direction.z += 1
+			print(camera_rotation)
+		elif camera_rotation == Vector3(0, -270, 0):
+			direction.x -= 1
+			print(camera_rotation)
 	
 	# Checking if the direction value has changed by inputs
 	if direction != Vector3.ZERO:
